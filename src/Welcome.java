eda6096086419cb2c2160a235ea3d400bbf1ddd1
AdmiN <AdmiN@ITWS066>
2024-08-20 10:18:20 +0530

    commit

---
+++ src/Welcome.java
@@ -0,0 +1,9 @@
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("welcome to devops CI/CD pipeleine project");
+	}
+
+}
